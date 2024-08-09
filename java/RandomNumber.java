ff3417bc9d8cfe51390f9c8e986fb31c1a9bdcee
shobna <shobna016@gmail.com>
2024-08-10 04:01:55 +0530

    hello

---
+++ java/RandomNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RandomNumber
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("Random doubles: " + Math.random());
+		System.out.println("Random doubles: " + Math.random());
+	System.out.println("Random doubles: " + Math.random());
+
+
+
+	}
+}
